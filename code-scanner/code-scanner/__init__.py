db4e5f7330f1bc3b40716f633e879148599fb00e
Mike Wang <mikenyc1207@gmail.com>
2020-05-29 18:24:00 -0400

    update some setup

---
+++ code-scanner/code-scanner/__init__.py
@@ -0,0 +1,5 @@
+from .hello import greeting
+
+__all__ = [
+    'greeting'
+]
---
+++ code-scanner/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='code-scanner',
+    packages=['code-scanner'],
+    version='1.0.0',
+    description='detector for essential code changes, generate a report after analysis',
+    author='wanzih',
+    author_email='..',
+    url='..',
+)
